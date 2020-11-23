b39a6391a81b2a4e3d7416b4031c06f1249e99b3
Krzysztof Kazanowski <72438796+Kazan33333@users.noreply.github.com>
2020-11-23 23:24:01 +0100

    Update Lab_3.cpp

--- Laboratorium 3/Lab_3.cpp
+++ Laboratorium 3/Lab_3.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <ctime>
+#include <iomanip>
 
 class Matrix {
 public:
@@ -41,15 +44,30 @@ Matrix::Matrix(int size)
     }
 }
 
-Matrix::Matrix(string path)
+Matrix::Matrix(std::string path)
 {
     std::fstream file;
-    file.open();
+    std::vector <double> tab;
+    file.open(path, std::ios::in);
+    if (file.good()) {
+        int rows, cols;
+        file >> rows >> cols;
+        int sth;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                file >> sth;
+                tab.push_back(sth);
+            }
+            matrix.push_back(tab);
+            tab.clear();
+        }
+    }
+    file.close();
 }
 
 Matrix::~Matrix(){}
 
-void Matrix::set(int n, int m, int val)
+void Matrix::set(int n, int m, double val)
 {
     matrix.at(n).at(m) = val;
 }
@@ -66,7 +84,7 @@ Matrix Matrix::add(Matrix &m2)
     Matrix result = Matrix(r, c);
     for (int i = 0; i < r; i++) {
         for (int j = 0; j < c; j++) {
-            (Matrix.matrix)[i][j] = matrix[i][j] + (m2.matrix)[i][j];
+            (result.matrix)[i][j] = matrix[i][j] + (m2.matrix)[i][j];
         }
     }
     return result;
@@ -79,7 +97,7 @@ Matrix Matrix::subtract(Matrix &m2)
     Matrix result = Matrix(r, c);
     for (int i = 0; i < r; i++) {
         for (int j = 0; j < c; j++) {
-            (Matrix.matrix)[i][j] = matrix[i][j] - (m2.matrix)[i][j];            
+            (result.matrix)[i][j] = matrix[i][j] - (m2.matrix)[i][j];            
         }
     }
     return result;
@@ -97,7 +115,7 @@ Matrix Matrix::multiply(Matrix &m2)
         for (int j = 0; j < m2c; j++) {
             sum = 0;
             for (int k = 0; k < c; k++) {
-                sum += matrix[i][k] * (m2matrix)[j][k];
+                sum += matrix[i][k] * (m2.matrix)[j][k];
             }
             (result.matrix)[i][j] = sum;
         }
@@ -118,14 +136,88 @@ int Matrix::rows()
 bool Matrix::store(std::string filename, std::string path)
 {
     std::fstream file;
+    if (!file.good()) {
+        return false;
+    }
+    int r = rows();
+    int c = cols();
+    file << "Wiersze: " << r << ", " << "Kolumny: " << c;
+    file << std::endl;
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            file << matrix[i][j];
+        }
+        file << std::endl;
+    }
+    file.close();
+    return true;
+}
+
+void Matrix::print()
+{    
+    int r = rows();
+    int c = cols();
+    for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++){
+            std::cout << std::setw(5) << matrix[i][j];
+        }
+        std::cout << std::endl;
+    }
 }
 
 int main()
 {
-    Matrix matrix1 = matrix(6,4);
-    Matrix matrix2 = matrix(4,6);
-    Matrix squarematrix1 = matrix(5);
-    Matrix squarematrix2 = matrix(5);
+    Matrix matrix1 = Matrix(6,4);
+    Matrix matrix2 = Matrix(4,6);
+    Matrix squarematrix1 = Matrix(5);
+    Matrix squarematrix2 = Matrix(5);
+    srand(time(NULL));
+    double tab;
+    for(int i = 0; i < 6; i++){
+        for(int j = 0; j < 4; j++){
+            tab = (double)(rand() % 100);
+            matrix1.set(i,j,tab);
+        }
+    }
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 6; j++){
+            tab = (double)(rand() % 100);
+            matrix2.set(i,j,tab);
+        }
+    }
+    for(int i = 0; i < 5; i++){
+        for(int j = 0; j < 5; j++){
+            tab = (double)(rand() % 100);
+            squarematrix1.set(i,j,tab);
+            tab = (double)(rand() % 100);
+            squarematrix2.set(i,j,tab);
+        }
+    }
+
+    std::cout << "1) Macierz 1 o wymiarach 6x4" << std::endl << std::endl;
+    matrix1.print();
+    std::cout << std::endl << "2) Macierz 2 o wymiarach 4x6" << std::endl << std::endl;
+    matrix2.print();
+    std::cout << std::endl << "3) Macierz kw. 1 o wymiarach 5x5" << std::endl << std::endl;
+    squarematrix1.print();
+    std::cout << std::endl << "4) Macierz kw. 2 o wymiarach 5x5" << std::endl << std::endl;
+    squarematrix2.print();
+    std::cout << std::endl << "5) Dodawanie:" << std::endl << std::endl;
+    Matrix result1 = squarematrix1.add(squarematrix2);
+    result1.print();
+    std::cout << std::endl << "6) Odejmowanie:" << std::endl << std::endl;
+    Matrix result2 = squarematrix1.subtract(squarematrix2);
+    result2.print();
+    std::cout << std::endl << "7) Mnozenie:" << std::endl << std::endl;
+    Matrix result3 = matrix1.multiply(matrix2) << std::endl;
+    result3.print();
+    std::cout << std::endl << "8) Zapis do pliku:" <<std::endl;
+    if(result3.store("File_store", "./Txt/")) {
+        std::cout << "The file has been saved" << std::endl;
+    }
+    std::cout << "--- Constructor from file ---" <<std::endl;
+    Matrix result4 = Matrix("./Txt/File");
+    result4.print();
 
     return 0;
 }
