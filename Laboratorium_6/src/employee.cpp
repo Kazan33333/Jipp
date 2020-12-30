d5b7a0b2df0520e9b25807211281a2e44fa7319c
Krzysztof Kazanowski <72438796+Kazan33333@users.noreply.github.com>
2020-12-30 18:13:07 +0100

    Update employee.cpp

--- Laboratorium_6/src/employee.cpp
+++ Laboratorium_6/src/employee.cpp
@@ -1,2 +1,37 @@
 #include "../include/lab6/employee.hpp"
 
+Employee::Employee(std::string ID, std::string name, std::string surname, std::string departmentID, std::string position){
+    this -> ID = ID;
+    this -> name = name;
+    this -> surname = surname;
+    this -> departmentID = departmentID;
+    this -> position = position;
+}
+
+std::string Employee::get_ID(){
+    return this -> ID;
+}
+
+std::string Employee::get_name(){
+    return this -> name;
+}
+
+std::string Employee::get_surname(){
+    return this -> surname;
+}
+
+std::string Employee::get_departmentID(){
+    return this -> departmentID;
+}
+
+std::string Employee::get_position(){
+    return this -> position;
+}
+
+void Employee::printEmployees(){
+    std::cout << "ID: " << this -> get_ID() << std::endl;
+    std::cout << "Name: " << this -> get_name() << std::endl;
+    std::cout << "Surname: " << this -> get_surname() << std::endl;
+    std::cout << "Position: " << this -> get_position() << std::endl;
+    std::cout << "Department ID: " << this -> get_departmentID() << std::endl;
+}
