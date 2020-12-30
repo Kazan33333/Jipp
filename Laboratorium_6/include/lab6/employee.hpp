8aadfebb76ec9d588cebc0ae860614d54e21358b
Krzysztof Kazanowski <72438796+Kazan33333@users.noreply.github.com>
2020-12-30 15:08:48 +0100

    Update employee.hpp

--- Laboratorium_6/include/lab6/employee.hpp
+++ Laboratorium_6/include/lab6/employee.hpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
-class employee {
-    friend class hrms;
+class Employee {
+    friend class HRMS;
     private:
         std::string ID;
         std::string name;
@@ -9,11 +9,12 @@ class employee {
         std::string departmentID;
         std::string position;
     public:
-        employee(std::string ID, std::string name, std::string surname, std::string departmentID, std::string position);
-        ~employee();
+        Employee(std::string ID, std::string name, std::string surname, std::string departmentID, std::string position);
+        ~Employee();
         std::string get_ID();
         std::string get_name();
         std::string get_surname();
         std::string get_departmentID();
         std::string get_position();
+        void printEmployees();
 };
