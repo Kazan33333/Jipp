13e812ccd34a0a08779cf7c52c125b984540ee92
Krzysztof Kazanowski <72438796+Kazan33333@users.noreply.github.com>
2020-12-30 18:14:16 +0100

    Update main.cpp

--- Laboratorium_6/src/main.cpp
+++ Laboratorium_6/src/main.cpp
@@ -3,5 +3,22 @@
 
 int main()
 {
-
+    Employee a("11", "Michael", "Scott", "D1", "Regional Manager");
+    Employee b("12", "Pam", "Beesly", "D1", "Office Administrator");
+    Employee c("13", "Darryl", "Philbin", "D1", "Warehouse Manager");
+    Employee d("21", "Jim", "Halpert", "D2", "Sales Representative");
+    Employee e("22", "Dwight", "Schrute", "D2", "Sales Representative");
+    Employee f("23", "Andy", "Bernard", "D2", "Sales Representative");
+    Employee g("24", "Stanley", "Hudson", "D2", "Sales Representative");
+    Employee h("25", "Phyllis", "Vance", "D2", "Sales Representative");
+    Employee i("31", "Angela", "Martin", "D3", "Senior Accountant");
+    Employee j("32", "Oscar", "Martinez", "D3", "Accountant");
+    Employee k("33", "Kevin", "Malone", "D3", "Accountant");
+    Employee l("41", "Creed", "Bratton", "D4", "Quality Assurance");
+    Employee m("42", "Kelly", "Kapoor", "D4", "Customer Service");
+    Employee n("43", "Meredith", "Palmer", "D4", "Supplier Relations");
+    Employee o("44", "Erin", "Hannon", "D4", "Receptionist");
+    Employee p("45", "Ryan", "Howard", "D4", "Temp");
+  
+    return 0;
 }
